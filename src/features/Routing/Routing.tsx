e8cd254c9feef10818046c73eae3545c4f11d1cc
Bogdan Tkach <tkachbogdan0205@gmail.com>
2022-12-04 17:40:06 +0200

    refactor: add merged conflicts

--- src/features/Routing/Routing.tsx
+++ src/features/Routing/Routing.tsx
@@ -1,16 +1,15 @@
 import {
   Route,
   Navigate,
-<<<<<<< HEAD
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import { Account } from "src/pages/Account";
+import { CreatePlaylistPage } from "src/pages/CreatePlaylistPage/CreatePlaylistPage";
 import { Profile } from "src/pages/Profile";
 import { AlbumsPage } from "../../pages/AlbumsPage";
 import { ArtistsPage } from "../../pages/ArtistsPage";
-import { CreatePlaylistPage } from "../../pages/CreatePlaylistPage";
 import { DownloadPage } from "../../pages/DownloadPage";
 import { Error404Page } from "../../pages/Error404Page";
 import { HomePage } from "../../pages/HomePage";
@@ -54,52 +53,3 @@ const router = createBrowserRouter(
 export const Routing = () => {
   return <RouterProvider router={router} />;
 };
-=======
-} from 'react-router-dom';
-import { Account } from 'src/pages/Account';
-import { Profile } from 'src/pages/Profile';
-import { AlbumsPage } from '../../pages/AlbumsPage';
-import { ArtistsPage } from '../../pages/ArtistsPage';
-import { CreatePlaylistPage } from '../../pages/CreatePlaylistPage/CreatePlaylistPage';
-import { DownloadPage } from '../../pages/DownloadPage';
-import { Error404Page } from '../../pages/Error404Page';
-import { HomePage } from '../../pages/HomePage';
-import { LikedSongsPage } from '../../pages/LikedSongsPage';
-import { LogIn } from '../../pages/LogIn';
-import { PlaylistPage } from '../../pages/PlaylistPage';
-import { PremiumSubscriptionPage } from '../../pages/PremiumSubscriptionPage';
-import { SearchPage } from '../../pages/SearchPage';
-import { SignUp } from '../../pages/SignUp';
-import { SupportPage } from '../../pages/SupportPage';
-
-export function Routing() {
-  return (
-    <Router>
-      <Routes>
-        {/* ASSIDE NAVBAR */}
-        <Route path='/' element={<HomePage />} />
-        <Route path='/search' element={<SearchPage />} />
-        <Route
-          path='/collection'
-          element={<Navigate to='/collection/playlists' replace={true} />}
-        />
-        <Route path='/collection/playlists' element={<PlaylistPage />} />
-        <Route path='/collection/artists' element={<ArtistsPage />} />
-        <Route path='/collection/albums' element={<AlbumsPage />} />
-        <Route path='/playlist/*' element={<CreatePlaylistPage />} />
-        <Route path='/collection/tracks' element={<LikedSongsPage />} />
-        {/* TOP NAVBAR */}
-        <Route path='/premium' element={<PremiumSubscriptionPage />} />
-        <Route path='/support' element={<SupportPage />} />
-        <Route path='/download/windows' element={<DownloadPage />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/user' element={<Profile />} />
-        <Route path='/download/windows' element={<DownloadPage />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<LogIn />} />
-        <Route path='*' element={<Error404Page />} />
-      </Routes>
-    </Router>
-  );
-}
->>>>>>> 453e594e4b47d65c527df0c9ec90df0f19591954
