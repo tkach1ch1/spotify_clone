1479b69d901a1c9834e7e611afffb7aef2ed4309
Bogdan Tkach <bogdantkach@MacBook-Air---Bogdan.local>
2023-01-10 18:48:56 +0200

    fix: fix audio is undefined error

--- src/layouts/NowPlayingBar/components/AuthorizedFooter/AuthorizedFooter.tsx
+++ src/layouts/NowPlayingBar/components/AuthorizedFooter/AuthorizedFooter.tsx
@@ -11,7 +11,7 @@ export const AuthorizedFooter = memo(() => {
     const audio = currentlyPlayingTrack?.file
 
     useEffect(() => {
-        if (audio.currentSrc !== '') {
+        if (audio && audio.currentSrc !== '') {
             if (isPlaying) {
                 audio.play()
             } else if (!isPlaying) {
